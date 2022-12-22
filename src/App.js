e1512809969aa11aa369e24606c741cd613f1c7b
studiomazia <studiomazia@gmail.com>
2022-12-22 22:58:09 +0500

    react hooks useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from "react";
 
-function App() {
+// const App = () => {
+//   const [details, setDetails] = useState({ counter: 0, name: "" });
+
+//   function increaseCounter() {
+//     setDetails((prev) => ({
+//       ...prev,
+//       counter: prev.counter + 1,
+//     }));
+//   }
+//   console.log(details);
+//   return (
+//     <div>
+//       <input type="text" onChange={(e) => e.target.value} />
+//       <h1>
+//         {details.name} has clicked {details.counter} times
+//       </h1>
+//       <button onClick={increaseCounter}>Increase</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+import React, { useState } from "react";
+
+const App = () => {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    address: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(details);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h3>Name:</h3> <input type="text" name="name" onChange={handleChange} />
+      <h3>Email:</h3>{" "}
+      <input type="email" name="email" onChange={handleChange} />
+      <h3>Password:</h3>{" "}
+      <input type="password" name="password" onChange={handleChange} />
+      <h3>Address:</h3>{" "}
+      <textarea onChange={handleChange} name="address"></textarea>
+      <button type="submit">Submit Form</button>
+    </form>
   );
-}
+};
 
 export default App;
